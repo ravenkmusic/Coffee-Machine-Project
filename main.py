366ff05bbb6ab149f0c094a6dfb25d5bfb8bf45f
ravenkmusic <ravenkbeats@gmail.com>
2022-02-21 09:42:00 -0500

    deleted menu module and combined with main

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
-from menu import menu
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit_so_far = 0
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
 print("Welcome to the Coffee Shop!")
-choice = input("What would you like?  ")
+
+coffee_machine = True
+
+while coffee_machine:
+    choice = input("What would you like? An espresso, a latte or a cappuccino? ")
+    if choice == "off":
+        coffee_machine = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit_so_far}")
